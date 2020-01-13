bd7ff15c9098cf40d673beb42c60343eda2c2554
Kalin Li <ChocolateQiaoli@outlook.com>
2020-01-13 18:07:26 +0800

    max/min value
    Resolve #1
    
    Signed-off-by: Kalin Li <ChocolateQiaoli@outlook.com>

--- test_demo.py
+++ test_demo.py
@@ -1,5 +1,12 @@
 from demo import *
+import random
 
-def test_foo():
-    res = foo()
-    assert(res == True)
+
+def test_Max():
+    a = []
+    for i in range(10):
+        a.append(random.randint(10, 100))
+
+    res = Max(a)
+    for x in a:
+        assert(res >= x)
